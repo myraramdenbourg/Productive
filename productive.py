9e9d32a25cdba1054222c740eb9dd5bcf9245f32
keanemind <keanemind@gmail.com>
2016-10-31 21:16:25 -0500

    Initial code (#3)
    * Initial code.
    
    * Removed non-code.
    
    * Removed 3rd menu choice.

---
+++ productive.py
@@ -0,0 +1,15 @@
+﻿choice = 0
+def menu():
+    print("""Productive.
+    (1) View Tasks
+    (2) Edit Tasks""")
+    choice = input("Select: ")
+
+menu()
+
+if choice == 1:
+    print("choice 1 selected")
+
+elif choice == 2:
+    print("choice 2 selected")
+
